dffcf1e0aaad1816586f53a7fb47afbd22ec2db1
valeriabukova <valeriasergeevna688@gmail.com>
2024-10-24 20:32:07 +0300

    class SlotMachine

--- one-armed banditvaleria/main.cpp
+++ one-armed banditvaleria/main.cpp
@@ -6,9 +6,80 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class SlotMachine
+{
+private:
+    char symbols[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
+    char reels[3];
+    int position;
+
+public:
+   
+    SlotMachine() : position(0) {}
+
+   
+    void spin()
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            reels[i] = symbols[(position + i) % 6];
+        }
+        position = (position + 1) % 6;
+    }
+
+   
+    void displayReels() const
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            cout << reels[i] << " ";
+        }
+        cout << endl;
+    }
+
+    
+    
+    bool checkWin() const
+    {
+        return (reels[0] == reels[1] && reels[1] == reels[2]);
+    }
+
+    
+    void showResult() const
+    {
+        if (checkWin())
+        {
+            cout << "YOU WIN!" << endl;
+        }
+        else
+        {
+            cout << "You lost. Try again!" << endl;
+        }
+    }
+};
+
+int main()
+{
+    SlotMachine machine;
+    char playAgain;
+
+    do
+    {
+        cout << "Press Enter to spin the reels...";
+        cin.get();
+
+        machine.spin();
+        machine.displayReels();
+
+        machine.showResult();
+
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+        cin.ignore();
+    }
+    while (playAgain == 'y' || playAgain == 'Y');  // Повтор игры, если выбрано 'y'
+
     return 0;
 }
