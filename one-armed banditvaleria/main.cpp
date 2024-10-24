25cf4ec7d2b229a6439760f88577600873975f2f
valeriabukova <valeriasergeevna688@gmail.com>
2024-10-24 20:20:18 +0300

    Initial Commit

---
+++ one-armed banditvaleria/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  one-armed banditvaleria
+//
+//  Created by Valeria  Bukova on 24.10.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
